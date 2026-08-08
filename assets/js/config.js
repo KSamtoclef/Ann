867d3a5187eb41cd510ef6d7b7369fb068f42b5b
KSamtoclef <samtoclef@gmail.com>
2026-08-08 23:03:05 +0100

    Configure Supabase registration storage

--- assets/js/config.js
+++ assets/js/config.js
@@ -2,5 +2,10 @@ window.ANN_CONFIG={
   processingMs:2400,
   activationMs:2200,
   shareMessage:'🎉 MTN at 25: Get 25GB to Celebrate!\n\nCelebrate 25 years of MTN with 25GB of data.\n\nClaim yours now!👇',
-  finalUrl:''
+  finalUrl:'',
+  supabase:{
+    url:'https://cqnovqvmxwmfngupgtov.supabase.co',
+    key:'sb_publishable_wm6kcw1pDhtyyORf0gLbVA_icVzmQ95',
+    table:'anniversary_registrations'
+  }
 };
