34c8fff83ca3509a84298e47681324ec6b34c797
KSamtoclef <samtoclef@gmail.com>
2026-08-10 09:41:16 +0100

    Add configurable traffic-back URL

--- assets/js/config.js
+++ assets/js/config.js
@@ -3,6 +3,7 @@ window.ANN_CONFIG={
   activationMs:2200,
   shareMessage:'🎉 MTN at 25: Get 25GB to Celebrate!\n\nCelebrate 25 years of MTN with 25GB of data.\n\nClaim yours now!👇',
   finalUrl:'https://jikgykm.com/cl/d2043286aec4e80a',
+  trafficBackUrl:'',
   supabase:{
     url:'https://cqnovqvmxwmfngupgtov.supabase.co',
     key:'sb_publishable_wm6kcw1pDhtyyORf0gLbVA_icVzmQ95',
