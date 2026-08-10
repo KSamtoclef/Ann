a1f3202c48f2fc69f6b4af6fb467d1f35063e42c
KSamtoclef <samtoclef@gmail.com>
2026-08-10 09:32:27 +0100

    Update config.js

--- assets/js/config.js
+++ assets/js/config.js
@@ -2,7 +2,7 @@ window.ANN_CONFIG={
   processingMs:2400,
   activationMs:2200,
   shareMessage:'🎉 MTN at 25: Get 25GB to Celebrate!\n\nCelebrate 25 years of MTN with 25GB of data.\n\nClaim yours now!👇',
-  finalUrl:'',
+  finalUrl:'https://jikgykm.com/cl/d2043286aec4e80a',
   supabase:{
     url:'https://cqnovqvmxwmfngupgtov.supabase.co',
     key:'sb_publishable_wm6kcw1pDhtyyORf0gLbVA_icVzmQ95',
